adeed956dd9ffbef8ff41c7c74e65f09e95bb7ca
Nowin <50371891+Nowin18@users.noreply.github.com>
2020-04-20 13:51:04 +0200

    Add files via upload

--- Shop.cpp
+++ Shop.cpp
@@ -6,6 +6,7 @@
 #include<ctime>
 #include<cstdlib>
 
+
 #include "Shop.h"
 
 void Shop::initWindow(){
@@ -22,35 +23,195 @@ Shop::~Shop(){
 
 void Shop::updateSFMLEvents(){
 	
-		while (this->window->pollEvent(this->sfEvent)) {
-			if (this->sfEvent.type == sf::Event::Closed) {
-				this->window->close();
-			}
+	
+	while (this->window->pollEvent(this->sfEvent)) {
+		switch (sfEvent.type) {
+
+		case sf::Event::Closed:
+			this->window->close();
+			break;
+		case sf::Event::MouseButtonPressed:
+			//tu dac if na poszczegolne przyciski
+			
+			
+			Shop::klik1();
+			break;
+		
 		}
+	}
+
 }
 
 void Shop::update(){
 	this->updateSFMLEvents();
 }
+void Shop::klik1()
+{
+	sf::Mouse mouse;
+	sf::Vector2f position = sf::Vector2f(mouse.getPosition(*window));
+	if (position.x >= 10 && position.x <= 230 && position.y <= 1000 && position.y >= 950)
+	{
+		std::cout << "KLIK" << std::endl;
+		Shop::run();
+	}
+	if (position.x >= 240 && position.x <= 480 && position.y <= 1000 && position.y >= 950)
+	{
+		
+		Shop::NapojeSklep();
+	}
+	if (position.x >= 490 && position.x <= 730 && position.y <= 1000 && position.y >= 950)
+	{
 
-void Shop::loadTexture(const std::string &screen){
+		Shop::WarzywaSklep();
+	}
+	if (position.x >= 740 && position.x <= 975 && position.y <= 1000 && position.y >= 950)
+	{
+
+		Shop::NabialSklep();
+	}
+	if (position.x >= 975 && position.x <= 1200 && position.y <= 1000 && position.y >= 950)
+	{
+
+		Shop::PieczywoSklep();
+	}
+	
+	
+}
+
+void Shop::loadTexture(const std::string& screen){
 
 	this->t.loadFromFile(screen);
+	
 }
+void Shop::Strzala1(const std::string &strzal) {
+
+	this->st1.loadFromFile(strzal);
+	}
+void Shop::Strzala2(const std::string& strzal) {
+
+	this->st2.loadFromFile(strzal);
+}
+void Shop::Strzala3(const std::string& strzal) {
+	this->st3.loadFromFile(strzal);
+}
+void Shop::Strzala4(const std::string& strzal) {
+	this->st4.loadFromFile(strzal);
+}
+void Shop::Strzala5(const std::string& strzal) {
+	this->st5.loadFromFile(strzal);
+}
+void Shop::Strzala7(const std::string& strzal) {
+	this->st7.loadFromFile(strzal);
+}
+
+
 
 void Shop::render(){
 
 	sf::Sprite s(this->t);
+	
+	
+	//Strzalaka 1 Kasa
+	sf::Sprite st1(this->st1);
+	st1.setPosition(sf::Vector2f(10, 950));
+	st1.setScale(sf::Vector2f(0.5, 0.4));
+	//Strzalka 2 Napoje
+	sf::Sprite st2(this->st2);
+	st2.setPosition(sf::Vector2f(250, 950));
+	st2.setScale(sf::Vector2f(0.5, 0.4));
+	//Strzalka 3 Warzywka
+	sf::Sprite st3(this->st3);
+	st3.setPosition(sf::Vector2f(490, 950));
+	st3.setScale(sf::Vector2f(0.5, 0.4));
+	//Strzalka 4 Nabia³
+	sf::Sprite st4(this->st4);
+	st4.setPosition(sf::Vector2f(730, 950));
+	st4.setScale(sf::Vector2f(0.5, 0.4));
+	//Strzalka 5 Pieczywo
+	sf::Sprite st5(this->st5);
+	st5.setPosition(sf::Vector2f(970, 950));
+	st5.setScale(sf::Vector2f(0.5, 0.4));
+	//Strzalka 7 koszyk
+	sf::Sprite st7(this->st7);
+	st7.setPosition(sf::Vector2f(1800, 940));
+	st7.setScale(sf::Vector2f(0.15, 0.15));
+	st7.setColor( sf::Color(0, 254, 0));
+	
+
 	this->window->clear(sf::Color::White);
+	
 	window->draw(s);
+	window->draw(st1);
+	window->draw(st2);
+	window->draw(st3);
+	window->draw(st4);
+	window->draw(st5);
+	window->draw(st7);
 	this->window->display();
+	
+	this->Strzala1(ST1);
+	this->Strzala2(ST2);
+	this->Strzala3(ST3);
+	this->Strzala4(ST4);
+	this->Strzala5(ST5);
+	this->Strzala7(ST7);
 }
+
 
 void Shop::run(){
 	while (this->window->isOpen())
 	{
 		this->update();
 		this->loadTexture(MAIN_SCREEN);
+		
 		this->render();
+		
+
 	}
 }
+void Shop::NapojeSklep() {
+	while (this->window->isOpen())
+	{
+		this->update();
+		this->loadTexture(napojeBg);
+		
+		this->render();
+
+
+	}
+}
+void Shop::WarzywaSklep() {
+	while (this->window->isOpen())
+	{
+		this->update();
+		this->loadTexture(warzywaBg);
+		
+		this->render();
+
+
+	}
+}
+void Shop::NabialSklep() {
+	while (this->window->isOpen())
+	{
+		this->update();
+		this->loadTexture(nabialBg);
+		
+		this->render();
+
+
+	}
+}
+void Shop::PieczywoSklep() {
+	while (this->window->isOpen())
+	{
+		this->update();
+		this->loadTexture(pieczywoBg);
+
+		this->render();
+
+
+	}
+}
+
+
--- Shop.h
+++ Shop.h
@@ -9,7 +9,22 @@
 #include<SFML/Window.hpp>
 #include<SFML/System.hpp>
 
-const std::string MAIN_SCREEN = "shop_layout.png";
+const std::string MAIN_SCREEN = "img/shop_layout.png";
+const std::string tlo1 = "img/tlo.jpg";
+const std::string napojeBg = "img/Napoje.jpg";
+const std::string warzywaBg = "img/warzywa.jpg";
+const std::string pieczywoBg = "img/pieczywo.jpg";
+const std::string nabialBg = "img/nabial.jpg";
+const std::string ST1 = "img/button1.png";
+const std::string ST2 = "img/button2.png";
+const std::string ST3 = "img/button3.png";
+const std::string ST4 = "img/button4.png";
+const std::string ST5 = "img/button5.png";
+const std::string ST6 = "img/button6.png";
+const std::string ST7 = "img/koszyk.png";
+
+
+
 
 //dwie inne czesci sklepu, na razie niedostepne
 /*const std::string RIGHT_SCREEN = "shop_layout.png";
@@ -20,19 +35,40 @@ class Shop
 {
 	sf::RenderWindow *window;
 	sf::Event sfEvent;
-
+	
 	sf::Texture t;
+	sf::Texture st1;
+	sf::Texture st2;
+	sf::Texture st3;
+	sf::Texture st4;
+	sf::Texture st5;
+	sf::Texture st6;
+	sf::Texture st7;
 
 	void initWindow();
 public:
 
 	Shop();
 	virtual ~Shop();
-
+	void klik1();
 	void updateSFMLEvents();
 	void update();
-	void loadTexture(const std::string &screen);
+	void loadTexture(const std::string& screen);
 	void render();
 	void run();
+	void NapojeSklep();
+	void WarzywaSklep();
+	void NabialSklep();
+	void PieczywoSklep();
+	void Strzala1(const std::string &strzal);
+	void Strzala2(const std::string& strzal);
+	void Strzala3(const std::string& strzal);
+	void Strzala4(const std::string& strzal);
+	void Strzala5(const std::string& strzal);
+	void Strzala6(const std::string& strzal);
+	void Strzala7(const std::string& strzal);
 };
+
+
 #endif
+//const std::string& screen
